09a1d1382d9f44cc17c9944f4e29cc05d688c2ad
Randall Burgess <randallallenburgess@gmail.com>
2020-07-28 14:33:45 -0700

    added weather, location and date boxes.  Added date builder function

--- src/App.js
+++ src/App.js
@@ -7,13 +7,44 @@ const api = {
 
 
 function App() {
+
+const dateBuilder = (d) => {
+  let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  let days = ["Monday", "Tuesday", "Wednesday", "thursday", "Friday", "Saturday", "Sunday"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year= d.getFullYear();
+
+  return `${day} ${date} ${month} ${year}`;
+}
+
   return (
     <div className="app">
       <main>
+        
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Search...">
-            
+
           </input>
+        </div>
+        
+        <div className="location-box">
+            <div className="location">
+              Seattle, US
+            </div>
+            <div className="date">
+                {dateBuilder(new Date())}
+            </div>
+        </div>
+
+        <div className="weather-box">
+          <div className="temp">
+            15°c
+          </div>
+
+          <div className="weather">Sunny</div>
         </div>
       </main>
     </div>
